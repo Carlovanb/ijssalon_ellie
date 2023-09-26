eb7b953e402e63d486fec85e54dc785d9b1a7003
Carlo van Beckhoven <carlovanb.cvb@gmail.com>
2023-09-26 19:56:45 +0200

    huiswerk les 10

---
+++ presentatie.py
@@ -0,0 +1,11 @@
+def presenteer(invoer_dict, totaal):
+    for item, bedrag in invoer_dict.items():
+        print(f"{item} : {bedrag} euro")
+    
+    print("==================")
+    print(f"totaal : {totaal} euro")
+       
+mijn_dict = {'vis': 10, 'vlees': 25, 'overig': 15}
+totaal = 50
+
+presenteer(mijn_dict, totaal) 
